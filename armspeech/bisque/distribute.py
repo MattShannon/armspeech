c073cbf12b791f84375c40bc19c3e44892c066d1
Matt Shannon <matt.shannon@cantab.net>
2012-10-04 17:56:56 +0100

    distribute - use new codeDeps framework for defining artifact and job hashes
    Ignore-this: 4c7540ede3d8fd47610f0d1a072df16
    
    darcs-hash:20121004165656-3a4db-355a0631990d336555cdf0acb8304a6604a524f8

--- armspeech/bisque/distribute.py
+++ armspeech/bisque/distribute.py
@@ -8,6 +8,7 @@
 
 from __future__ import division
 
+import codedep
 from armspeech.util import persist
 from codedep import codeDeps, ForwardRef
 
@@ -27,42 +28,6 @@ import modulefinder
 #   in the pickle of the artifact B which is used to compute the hash of B, but
 #   may affect the value of A and B.)
 #   This bug needs to be fixed.
-
-@codeDeps()
-def findDeps(srcFile):
-    """Returns local dependencies for a given source file.
-
-    Local means defined under PYTHONPATH (or in the same directory as the given
-    source file if PYTHONPATH not defined), the idea being that local files are
-    the ones subject to change and need to be hashed.
-    It is assumed that modules that are on the system search path are fixed.
-    """
-
-    # FIXME : this is broken in many ways. Firstly, an exception should
-    #   probably be raised (or at least a warning printed) if there are any
-    #   import errors. Currently all standard modules cause import errors due
-    #   to the fact [envPythonPath] contains only the armspeech path. The path
-    #   should probably be left blank instead, but this requires the returned
-    #   list of module files to be pruned to only contain those that are below
-    #   envPythonPath. Secondly, relative imports appear to fail in lots of
-    #   circumstances (not sure exactly which). One fix to this problem would
-    #   be to create a temporary file containing 'import armspeech.whatever'
-    #   but this is a bit ugly and requires finding the fully-qualified module
-    #   name (which is probably not hard). Alternatively it may be possible to
-    #   use modulefinder in a better way (load_file is perhaps a bit better than
-    #   run_script but still has the relative import problem). Finally, it would
-    #   be nice for our use case to be able to use modulefinder on several files
-    #   at once. Think we can just call run_script or load_file multiple times,
-    #   but need to check.
-    #
-    #   All these issues will go away once we stop using findDeps (imminent).
-
-    # FIXME : does sys.path[0] ever do anything here? Should just fail instead?
-    envPythonPath = os.environ['PYTHONPATH'] if 'PYTHONPATH' in os.environ else sys.path[0]
-    finder = modulefinder.ModuleFinder(path = [envPythonPath])
-    finder.run_script(srcFile)
-    depFiles = [ mod.__file__ for modName, mod in finder.modules.items() if mod.__file__ is not None ]
-    return sorted(depFiles)
 
 @codeDeps(ForwardRef(lambda: ancestorArtifacts), persist.secHashObject)
 class Artifact(object):
@@ -85,7 +50,7 @@ class FixedArtifact(Artifact):
     def loc(self, baseDir):
         return self.location
 
-@codeDeps(Artifact, findDeps, persist.secHashFile)
+@codeDeps(Artifact, codedep.getHash)
 class JobArtifact(Artifact):
     def __init__(self, parentJob):
         self.parentJob = parentJob
@@ -96,7 +61,7 @@ class JobArtifact(Artifact):
     def secHashExternals(self):
         return []
     def secHashSources(self):
-        return [ persist.secHashFile(depFile) for depFile in findDeps(inspect.getsourcefile(self.parentJob.__class__)) ]
+        return [codedep.getHash(self.parentJob.__class__)]
     def loc(self, baseDir):
         return os.path.join(baseDir, self.secHash())
 
