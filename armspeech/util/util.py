eb4684847565ff9f2a05d7e18a2a0b488a6044d6
Matt Shannon <matt.shannon@cantab.net>
2012-10-11 07:40:02 +0100

    (util - add some helper functions)
    Ignore-this: 73816ff1957a6f4c85e44bcc7ba55983
    
    darcs-hash:20121011064002-3a4db-f4954d1b7011a884def64a49947f8799e2161138

--- armspeech/util/util.py
+++ armspeech/util/util.py
@@ -11,6 +11,29 @@ from __future__ import division
 from codedep import codeDeps
 
 @codeDeps()
+def identityFn(x):
+    return x
+
+@codeDeps()
+class ConstantFn(object):
+    def __init__(self, value):
+        self.value = value
+
+    def __call__(self, *args, **kwargs):
+        return self.value
+
+@codeDeps()
+def getElem(xs, index, length, default = None):
+    try:
+        lengthActual = len(xs)
+    except TypeError:
+        # not a sequence type
+        return default
+    if lengthActual != length:
+        return default
+    return xs[index]
+
+@codeDeps()
 def orderedDictRepr(orderedKeys, dictIn):
     assert len(orderedKeys) == len(dictIn)
     return '{'+', '.join([ repr(key)+': '+repr(dictIn[key]) for key in orderedKeys ])+'}'
