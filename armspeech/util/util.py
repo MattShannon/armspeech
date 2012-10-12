d92ce324a0cbcf3d29a8ed1e57b29c61b5c92892
Matt Shannon <matt.shannon@cantab.net>
2012-10-12 18:26:02 +0100

    util - add ElemGetter and AttrGetter
    Ignore-this: 68fa15d163e6fd7c48606226ba4c2535
    
    darcs-hash:20121012172602-3a4db-672c942405022ee0be034039fcf23d8a14cc329b

--- armspeech/util/util.py
+++ armspeech/util/util.py
@@ -34,6 +34,35 @@ def getElem(xs, index, length, default = None):
     return xs[index]
 
 @codeDeps()
+class ElemGetter(object):
+    """Picklable item getter with length check."""
+    def __init__(self, index, length):
+        self.index = index
+        self.length = length
+
+    def __repr__(self):
+        return 'ElemGetter(%s, %s)' % (self.index, self.length)
+
+    def __call__(self, xs):
+        lengthActual = len(xs)
+        if lengthActual != self.length:
+            raise RuntimeError('sequence of length %s expected (%s actual)' %
+                               (self.length, lengthActual))
+        return xs[self.index]
+
+@codeDeps()
+class AttrGetter(object):
+    """Picklable attribute getter."""
+    def __init__(self, attr):
+        self.attr = attr
+
+    def __repr__(self):
+        return 'AttrGetter(%s)' % self.attr
+
+    def __call__(self, obj):
+        return getattr(obj, self.attr)
+
+@codeDeps()
 def orderedDictRepr(orderedKeys, dictIn):
     assert len(orderedKeys) == len(dictIn)
     return '{'+', '.join([ repr(key)+': '+repr(dictIn[key]) for key in orderedKeys ])+'}'
