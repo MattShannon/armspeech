b5f23463764368bc94d409498e50ac3be479ab18
Matt Shannon <matt.shannon@cantab.net>
2012-10-15 21:50:13 +0100

    add corpus module to armspeech.modelling.bisque
    Ignore-this: 316b198d427fa9a9d7f3b00486385e7b
    
    darcs-hash:20121015205013-3a4db-0080f81150ca6803ae753d6f9c662698f33944cf

---
+++ armspeech/modelling/bisque/corpus.py
@@ -0,0 +1,23 @@
+"""Corpus helper functions for distributed jobs."""
+
+# Copyright 2011, 2012 Matt Shannon
+
+# This file is part of armspeech.
+# See `License` for details of license and warranty.
+
+
+from __future__ import division
+
+from armspeech.util.iterhelp import chunkList
+from armspeech.bisque.distribute import liftLocal, lit, lift
+from codedep import codeDeps
+
+@codeDeps(chunkList)
+def getUttIdChunks(corpus, numChunks):
+    return chunkList(corpus.trainUttIds, numChunks)
+
+@codeDeps(getUttIdChunks, liftLocal, lit)
+def getUttIdChunkArts(corpusArt, numChunksLit = lit(1)):
+    numChunks = numChunksLit.litValue
+    return liftLocal(getUttIdChunks, numOut = numChunks)(corpusArt,
+                                                         numChunksLit)
