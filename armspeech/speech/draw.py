ef0a7eb6660cf0930b20912ee5f1eec0ec94bf43
Matt Shannon <matt.shannon@cantab.net>
2013-05-17 14:47:13 +0100

    (draw - minor code layout improvements)
    Ignore-this: 9b1c278450b96d01751065b87bda7be3
    
    darcs-hash:20130517134713-3a4db-d7171087d47826f7503a88968c5b83729f0537bb

--- armspeech/speech/draw.py
+++ armspeech/speech/draw.py
@@ -25,22 +25,29 @@ def partitionSeq(xs, numPartitions):
     return out
 
 @codeDeps()
-def drawLabelledSeq(dataSeqs, labelSeqs, outPdf, figSizeRate = None, fillBetween = [], xmin = None, xmax = None, ylims = None, xlabel = None, ylabel = None, legend = None, colors = ['red', 'purple', 'orange', 'blue']):
+def drawLabelledSeq(dataSeqs, labelSeqs, outPdf, figSizeRate = None,
+                    fillBetween = [], xmin = None, xmax = None, ylims = None,
+                    xlabel = None, ylabel = None, legend = None,
+                    colors = ['red', 'purple', 'orange', 'blue']):
     import matplotlib.pyplot as plt
 
     if xmin is None:
-        xmin = min([ dataSeq[0][0] for dataSeq in dataSeqs ] + [ labelSeq[0][0] for labelSeq in labelSeqs ])
+        xmin = min([ dataSeq[0][0] for dataSeq in dataSeqs ] +
+                   [ labelSeq[0][0] for labelSeq in labelSeqs ])
     if xmax is None:
-        xmax = max([ dataSeq[0][-1] for dataSeq in dataSeqs ] + [ labelSeq[-1][1] for labelSeq in labelSeqs ])
+        xmax = max([ dataSeq[0][-1] for dataSeq in dataSeqs ] +
+                   [ labelSeq[-1][1] for labelSeq in labelSeqs ])
 
     if figSizeRate is None:
         fig = plt.figure()
     else:
-        fig = plt.figure(figsize = ((xmax - xmin) * figSizeRate, 6.0), dpi = 300.0)
+        fig = plt.figure(figsize = ((xmax - xmin) * figSizeRate, 6.0),
+                         dpi = 300.0)
     ax = fig.add_subplot(1, 1, 1)
 
     for x, y1, y2 in fillBetween:
-        ax.fill_between(x, y1, y2, color = 'blue', facecolor = 'blue', linewidth = 0.2, alpha = 0.2)
+        ax.fill_between(x, y1, y2, color = 'blue', facecolor = 'blue',
+                        linewidth = 0.2, alpha = 0.2)
     for x, y in dataSeqs:
         ax.plot(x, y, '-')
 
@@ -52,8 +59,13 @@ def drawLabelledSeq(dataSeqs, labelSeqs, outPdf, figSizeRate = None, fillBetween
         for start, end, label in labelSeq:
             pos = (start + end) / 2.0
             if xmin < pos < xmax:
-                ax.text(pos, height + 0.02, label, transform = trans, horizontalalignment = 'center', verticalalignment = 'center', fontsize = 8)
-            ax.axvspan(xmin = start, xmax = end, ymin = height + 0.0, ymax = height + 0.04, facecolor = color, alpha = 0.5)
+                ax.text(pos, height + 0.02, label, transform = trans,
+                        horizontalalignment = 'center',
+                        verticalalignment = 'center',
+                        fontsize = 8)
+            ax.axvspan(xmin = start, xmax = end,
+                       ymin = height + 0.0, ymax = height + 0.04,
+                       facecolor = color, alpha = 0.5)
             ax.axvline(x = start, linewidth = 0.1, color = 'black')
 
     ax.set_xlim(xmin, xmax)
@@ -77,7 +89,8 @@ def drawWarping(transformList, outPdf, xlims, ylims = None, title = None):
     plt.figure()
     xs = np.linspace(xmin, xmax, 101)
     if len(transformList) == 0:
-        print 'NOTE: no transforms being drawn for drawWarping with outPdf =', outPdf
+        print ('NOTE: no transforms being drawn for drawWarping with'
+               ' outPdf = %s' % outPdf)
     for transform in transformList:
         ys = [ transform(x) for x in xs ]
         plt.plot(xs, ys, '-')
@@ -110,7 +123,8 @@ def drawLogPdf(outputs, bins, outPdf, fns = [], ylims = None, title = None):
     plt.savefig(outPdf)
 
 @codeDeps(d.SynthMethod)
-def drawFor1DInput(debugAcc, subDist, outPdf, xlims, ylims, title = None, drawPlusMinusTwoStdev = False):
+def drawFor1DInput(debugAcc, subDist, outPdf, xlims, ylims, title = None,
+                   drawPlusMinusTwoStdev = False):
     """Draws plot showing several things of interest for the case of 1D input.
 
     For both debugAcc and subDist, input should be a 1D vector and output
@@ -121,10 +135,12 @@ def drawFor1DInput(debugAcc, subDist, outPdf, xlims, ylims, title = None, drawPl
     def subDrawScatter(inputs, outputs):
         for input in inputs:
             if len(input) != 1:
-                raise RuntimeError('input should be a vector of length 1, but was '+repr(input))
+                raise RuntimeError('input should be a vector of length 1,'
+                                   ' but was %r' % input)
         for output in outputs:
             if np.shape(output) != ():
-                raise RuntimeError('output should be a scalar, but was '+repr(output))
+                raise RuntimeError('output should be a scalar,'
+                                   ' but was %r' % output)
         if len(inputs) == 0:
             xs = np.zeros([0])
         else:
@@ -132,17 +148,24 @@ def drawFor1DInput(debugAcc, subDist, outPdf, xlims, ylims, title = None, drawPl
         ys = np.array(outputs)
         plt.plot(xs, ys, '.', markersize = 0.2)
 
-    def subDrawMeanish(subDist, xlims, nx = 50, drawPlusMinusTwoStdev = False, numSamples = 200):
+    def subDrawMeanish(subDist, xlims, nx = 50, drawPlusMinusTwoStdev = False,
+                       numSamples = 200):
         xmin, xmax = xlims
         xs = np.linspace(xmin, xmax, nx + 1)
-        ysMeanish = [ subDist.synth(np.array([x]), method = d.SynthMethod.Meanish) for x in xs ]
+        ysMeanish = [
+            subDist.synth(np.array([x]), method = d.SynthMethod.Meanish)
+            for x in xs
+        ]
         plt.plot(xs, ysMeanish, '-')
         if drawPlusMinusTwoStdev:
             ysSampleLow = []
             ysSampleMiddle = []
             ysSampleHigh = []
             for x in xs:
-                samples = [ subDist.synth(np.array([x]), method = d.SynthMethod.Sample) for i in range(numSamples) ]
+                samples = [
+                    subDist.synth(np.array([x]), method = d.SynthMethod.Sample)
+                    for i in range(numSamples)
+                ]
                 m = np.mean(samples)
                 sd = np.std(samples)
                 ysSampleLow.append(m - 2.0 * sd)
@@ -157,12 +180,22 @@ def drawFor1DInput(debugAcc, subDist, outPdf, xlims, ylims, title = None, drawPl
         ymin, ymax = ylims
         dx = (xmax - xmin) / nx
         dy = (ymax - ymin) / ny
-        pdfValues = [[ math.exp(subDist.logProb(np.array([x]), y)) for x in np.linspace(xmin, xmax, nx + 1) ] for y in np.linspace(ymin, ymax, ny + 1) ]
-        plt.imshow(pdfValues, extent = [xmin - 0.5 * dx, xmax - 0.5 * dx, ymin - 0.5 * dy, ymax - 0.5 * dy], origin = 'lower', cmap = plt.cm.gray, interpolation = 'nearest')
+        pdfValues = [ [ math.exp(subDist.logProb(np.array([x]), y))
+                        for x in np.linspace(xmin, xmax, nx + 1) ]
+                      for y in np.linspace(ymin, ymax, ny + 1) ]
+        plt.imshow(
+            pdfValues,
+            extent = [xmin - 0.5 * dx, xmax - 0.5 * dx,
+                      ymin - 0.5 * dy, ymax - 0.5 * dy],
+            origin = 'lower',
+            cmap = plt.cm.gray,
+            interpolation = 'nearest'
+        )
 
     plt.figure()
     subDrawScatter(debugAcc.memo.inputs, debugAcc.memo.outputs)
-    subDrawMeanish(subDist, xlims, drawPlusMinusTwoStdev = drawPlusMinusTwoStdev)
+    subDrawMeanish(subDist, xlims,
+                   drawPlusMinusTwoStdev = drawPlusMinusTwoStdev)
     subDrawPdfImage(subDist, xlims, ylims)
     plt.xlim(*xlims)
     plt.ylim(*ylims)
