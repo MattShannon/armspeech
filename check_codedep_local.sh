f87e3ac86f9b510ee977062eab508aaa226f15c8
Matt Shannon <matt.shannon@cantab.net>
2013-07-12 22:26:45 +0100

    add local version of check_codedep.sh tool which is a bit cleverer for this repo
    Ignore-this: c80ac6b7820c3ee5ea7cc166fa7030f3
    
    darcs-hash:20130712212645-3a4db-c30b77c9d10af4c6a6bbc168b91f77493a3cba4c

---
+++ check_codedep_local.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+if [[ "`dirname "$0"`" != "." ]]; then
+    echo "check_codedep.sh: this tool is designed to be run from the current directory" 1>&2
+    exit 1
+fi
+
+tmpDir=`mktemp -d`
+echo "(using temporary dir $tmpDir)"
+
+for pyFile in "$@"; do
+    moduleName="`echo "$pyFile" | sed -r 's%/%.%g;s/\.py$//'`"
+    echo
+    echo "(moduleName = $moduleName, pyFile = $pyFile)"
+    echo
+    pyFileNew="$tmpDir"/"`basename "$pyFile"`"
+
+    PYTHONPATH=. python check_codedep.py "$moduleName" > "$pyFileNew"
+
+    # below stanza is to ensure eval_local receives correct special treatment
+    diffLineExpected=`{ grep -n '^def eval_local(' "$pyFile" || true; } | sed -r 's/:.*//'`
+    if [[ "$diffLineExpected" == "" ]]; then
+        diffExpected=""
+    else
+        diffExpected="`{ echo "$(( diffLineExpected - 1 ))a$diffLineExpected"; echo "> @codeDeps()"; }`"
+    fi
+
+    if [[ "`diff "$pyFile" "$pyFileNew"`" != "$diffExpected" ]]; then
+        vimdiff "$pyFile" "$pyFileNew"
+    else
+        echo "(no change)"
+    fi
+
+    rm -f "$pyFileNew"
+    echo
+done
+
+rmdir "$tmpDir" && echo "(cleaned-up temporary dir $tmpDir)"
