60b067dc7776e0f3fe7fe383b631d3c879f9d73c
Matt Shannon <matt.shannon@cantab.net>
2013-07-12 22:26:30 +0100

    add tool to automate check_codedep a bit more
    Ignore-this: 36f5c91d1f726c9f8b5d4bf6c68ff017
    
    darcs-hash:20130712212630-3a4db-191d2b72992b2904d9000216fec4088f5ac532e5

---
+++ check_codedep.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+if [[ "`dirname "$0"`" != "." ]]; then
+    echo "check_codedep.sh: this tool is designed to be run from the current directory" 1>&2
+    exit 1
+fi
+
+tmpDir=`mktemp -d`
+echo "(using temporary dir $tmpDir)"
+
+for pyFile in "$@"; do
+    moduleName="`echo "$pyFile" | sed -r 's%/%.%g;s/\.py$//'`"
+    echo
+    echo "(moduleName = $moduleName, pyFile = $pyFile)"
+    echo
+    pyFileNew="$tmpDir"/"`basename "$pyFile"`"
+
+    PYTHONPATH=. python check_codedep.py "$moduleName" > "$pyFileNew"
+
+    if [[ "`diff -q "$pyFile" "$pyFileNew"`" != "" ]]; then
+        vimdiff "$pyFile" "$pyFileNew"
+    else
+        echo "(no change)"
+    fi
+
+    rm -f "$pyFileNew"
+    echo
+done
+
+rmdir "$tmpDir" && echo "(cleaned-up temporary dir $tmpDir)"
