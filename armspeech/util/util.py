499c8480c79e5c75ccc671a8ac405293a6e8dec7
Matt Shannon <matt.shannon@cantab.net>
2013-07-12 20:47:09 +0100

    (util - add MapElem)
    Ignore-this: 1a3629ed12a682db8761614624496376
    
    darcs-hash:20130712194709-3a4db-80224a65de2f38ae4956a615c9a96a9497bf5d7f

--- armspeech/util/util.py
+++ armspeech/util/util.py
@@ -79,6 +79,29 @@ class AndThen(object):
         return self.second(y)
 
 @codeDeps()
+class MapElem(object):
+    """Maps one element of a tuple."""
+    def __init__(self, index, length, fn):
+        self.index = index
+        self.length = length
+        self.fn = fn
+
+        assert 0 <= self.index < self.length
+
+    def __repr__(self):
+        return 'MapElem(%r, %r, %r)' % (self.index, self.length, self.fn)
+
+    def __call__(self, xs):
+        lengthActual = len(xs)
+        if lengthActual != self.length:
+            raise RuntimeError('sequence of length %s expected (%s actual)' %
+                               (self.length, lengthActual))
+        xs = list(xs)
+        xs[self.index] = self.fn(xs[self.index])
+        xs = tuple(xs)
+        return xs
+
+@codeDeps()
 def orderedDictRepr(orderedKeys, dictIn):
     assert len(orderedKeys) == len(dictIn)
     return '{'+', '.join([ repr(key)+': '+repr(dictIn[key]) for key in orderedKeys ])+'}'
