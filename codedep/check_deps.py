9657d7d5d925eb9a3e96576d0006135d88140104
Matt Shannon <matt.shannon@cantab.net>
2013-07-17 00:46:10 +0100

    (check_deps - code cosmetic changes)
    Ignore-this: 6a89a1b0e98acf74c1aae60a49bad936
    
    darcs-hash:20130716234610-3a4db-d321cc38e9985b8760ba196fff6947bd6d5ea21f

--- codedep/check_deps.py
+++ codedep/check_deps.py
@@ -17,13 +17,18 @@ import ast
 import symtable
 import importlib
 
-def peekIter(it):
-    it = iter(it)
-    elem = it.next()
+def peekIter(itr):
+    """Returns an iterator which can peek one element ahead.
+
+    If the input iterator represents a sequence of current elements, then the
+    returned iterator represents a sequence of (current, next) elements.
+    """
+    itr = iter(itr)
+    elem = itr.next()
     done = False
     while not done:
         try:
-            elemNext = it.next()
+            elemNext = itr.next()
         except StopIteration:
             elemNext = None
             done = True
@@ -35,8 +40,8 @@ def attachAstAndSymtab(nodes, symtab, depth = 0):
 
     A scope_depth attribute is added to each symtab giving the number of levels
     of nested scope above the current symtab.
-    A symtab_current_scope attribute is added to each AST node giving the symtab
-    for the scope that is current at that node.
+    A symtab_current_scope attribute is added to each AST node giving the
+    symtab for the scope that is current at that node.
     """
     symtab.scope_depth = depth
     symtabChildrenLeft = list(reversed(symtab.get_children()))
@@ -46,12 +51,13 @@ def attachAstAndSymtab(nodes, symtab, depth = 0):
 
 def attachAstAndSymtabSub(node, symtab, symtabChildrenLeft, depth):
     node.symtab_current_scope = symtab
-    if isinstance(node, (_ast.FunctionDef, _ast.ClassDef, _ast.Lambda, _ast.GeneratorExp)):
+    if isinstance(node, (_ast.FunctionDef, _ast.ClassDef, _ast.Lambda,
+                         _ast.GeneratorExp)):
         # new scope introduced, and used in some of the children
 
-        # (FIXME : order of descent into new scopes may not be correct (that is,
-        #   may disagree with ordering used by symtable) for complicated nested
-        #   scope cases. Need to think about.)
+        # (FIXME : order of descent into new scopes may not be correct (that
+        #   is, may disagree with ordering used by symtable) for complicated
+        #   nested scope cases. Need to think about.)
         if isinstance(node, _ast.FunctionDef):
             subNodesOldScope = node.args.defaults + node.decorator_list
             subNodesNewScope = node.args.args + node.body
@@ -65,19 +71,24 @@ def attachAstAndSymtabSub(node, symtab, symtabChildrenLeft, depth):
             subNodesNewScope = node.args.args + [node.body]
             # node.args itself would otherwise be missed out
             node.args.symtab_current_scope = symtab
-        elif isinstance(node, (_ast.SetComp, _ast.DictComp, _ast.GeneratorExp)):
+        elif isinstance(node, (_ast.SetComp, _ast.DictComp,
+                               _ast.GeneratorExp)):
             subNodesOldScope = [ subNode.iter for subNode in node.generators ]
             subNodesNewScope = (
-                ([node.key, node.value] if isinstance(node, _ast.DictComp) else [node.elt]) +
+                ([node.key, node.value] if isinstance(node, _ast.DictComp)
+                 else [node.elt]) +
                 [ subNode.target for subNode in node.generators ] +
-                [ subSubNode for subNode in node.generators for subSubNode in subNode.ifs ]
+                [ subSubNode
+                  for subNode in node.generators
+                  for subSubNode in subNode.ifs ]
             )
             # each node in node.generators would otherwise be missed out
             for subNode in node.generators:
                 subNode.symtab_current_scope = symtab
 
         for subNode in subNodesOldScope:
-            attachAstAndSymtabSub(subNode, symtab, symtabChildrenLeft, depth = depth)
+            attachAstAndSymtabSub(subNode, symtab, symtabChildrenLeft,
+                                  depth = depth)
 
         symtabChild = symtabChildrenLeft.pop()
         if isinstance(node, (_ast.FunctionDef, _ast.ClassDef)):
@@ -85,7 +96,8 @@ def attachAstAndSymtabSub(node, symtab, symtabChildrenLeft, depth):
         attachAstAndSymtab(subNodesNewScope, symtabChild, depth = depth + 1)
     else:
         for subNode in ast.iter_child_nodes(node):
-            attachAstAndSymtabSub(subNode, symtab, symtabChildrenLeft, depth = depth)
+            attachAstAndSymtabSub(subNode, symtab, symtabChildrenLeft,
+                                  depth = depth)
 
 def isGlobal(symtab, symName):
     """Returns True if symbol referred to by symName in symtab is global.
@@ -98,7 +110,9 @@ def isGlobal(symtab, symName):
 
 def findGlobalUses(node, onLoadGlobalFound):
     curr_symtab = node.symtab_current_scope
-    if isinstance(node, _ast.Call) and isinstance(node.func, _ast.Name) and node.func.id == 'codeDeps':
+    if True and (isinstance(node, _ast.Call) and
+                 isinstance(node.func, _ast.Name) and
+                 node.func.id == 'codeDeps'):
         # ignore names present in arguments to codeDeps
         for subNode in ast.iter_child_nodes(node):
             findGlobalUses(subNode, lambda name: ())
@@ -106,7 +120,9 @@ def findGlobalUses(node, onLoadGlobalFound):
         if isinstance(node.ctx, (_ast.Load, _ast.AugLoad)):
             onLoadGlobalFound(node.id)
         # no children
-    elif isinstance(node, _ast.Attribute) and isinstance(node.value, _ast.Name) and isGlobal(curr_symtab, node.value.id):
+    elif True and (isinstance(node, _ast.Attribute) and
+                   isinstance(node.value, _ast.Name) and
+                   isGlobal(curr_symtab, node.value.id)):
         if isinstance(node.ctx, (_ast.Load, _ast.AugLoad)):
             onLoadGlobalFound(node.value.id+'.'+node.attr)
         else:
@@ -142,21 +158,21 @@ def simpleAssignToName(node):
 
 def prettyPrintBisqueDepsStanza(deps, init = '@', maxLineLength = 80):
     if not deps:
-        return init+'codeDeps()'
+        return '%scodeDeps()' % init
     else:
-        ret = init+'codeDeps('+(', '.join(deps))+')'
+        ret = '%scodeDeps(%s)' % (init, ', '.join(deps))
         if len(ret) <= maxLineLength:
             return ret
         else:
             ret = ''
-            currLine = init+'codeDeps('+deps[0]+','
+            currLine = '%scodeDeps(%s,' % (init, deps[0])
             for dep in deps[1:]:
                 if len(currLine) + len(dep) + 2 <= maxLineLength:
-                    currLine += (' '+dep+',')
+                    currLine += (' %s,' % dep)
                 else:
                     ret += (currLine+'\n')
-                    currLine = '    '+dep+','
-            ret += currLine[:-1]+'\n)'
+                    currLine = '    %s,' % dep
+            ret += (currLine[:-1]+'\n)')
             return ret
 
 def main(args):
@@ -188,7 +204,7 @@ def main(args):
         loadGlobals = []
         def onLoadGlobalFound(name):
             if '.' in name:
-                nameLeft, nameRight = name.split('.', 1)
+                nameLeft, _ = name.split('.', 1)
                 nameLeftObj = eval(nameLeft, vars(module))
                 if inspect.ismodule(nameLeftObj):
                     loadGlobals.append(name)
@@ -214,7 +230,8 @@ def main(args):
                     newLoadGlobals.update(privateDeps[name])
                 else:
                     newLoadGlobals.add(name)
-                    sys.stderr.write('NOTE: treating %s as non-private\n' % name)
+                    sys.stderr.write('NOTE: treating %s as non-private\n' %
+                                     name)
             else:
                 newLoadGlobals.add(name)
         loadGlobalss[nodeIndex] = sorted(newLoadGlobals)
@@ -222,13 +239,17 @@ def main(args):
         # add current node to privateDeps if appropriate
         nameAssignedTo = simpleAssignToName(node)
         if nameAssignedTo is not None and nameAssignedTo.startswith('_'):
-            # statement is simple assignment to a private variable, i.e. of the form '_bla = ...'
+            # statement is simple assignment to a private variable,
+            #   i.e. of the form '_bla = ...'
             privateDeps[nameAssignedTo] = loadGlobals
-            sys.stderr.write('will expand %s to %s\n' % (nameAssignedTo, loadGlobals))
+            sys.stderr.write('will expand %s to %s\n' %
+                             (nameAssignedTo, loadGlobals))
 
     sys.stderr.write('\n')
     sys.stderr.write('RESOLVING LOCATIONS:\n')
-    namesDefinedInModule = set([ subNode for node in nodeModule.body for subNode in assignsNames(node) ])
+    namesDefinedInModule = set([ subNode
+                                 for node in nodeModule.body
+                                 for subNode in assignsNames(node) ])
 
     names = set()
     for node, loadGlobals in zip(nodeModule.body, loadGlobalss):
@@ -241,7 +262,7 @@ def main(args):
             pass
         else:
             if '.' in name:
-                nameLeft, nameRight = name.split('.', 1)
+                nameLeft, _ = name.split('.', 1)
                 nameLeftObj = eval(nameLeft, vars(module))
                 assert inspect.ismodule(nameLeftObj)
                 if hasattr(nameLeftObj, '__file__'):
@@ -253,16 +274,22 @@ def main(args):
                 try:
                     nameObj = eval(name, vars(module))
                 except NameError:
-                    sys.stderr.write('NOTE: %s refers to nothing (ignoring)\n' % name)
+                    sys.stderr.write('NOTE: %s refers to nothing'
+                                     ' (ignoring)\n' % name)
                     nameObj = None
                 if nameObj is None:
                     sourceFileRel = None
-                elif inspect.isbuiltin(nameObj) or getattr(nameObj, '__module__', None) == '__builtin__':
+                elif True and (inspect.isbuiltin(nameObj) or
+                               (getattr(nameObj, '__module__', None) ==
+                                '__builtin__')):
                     sourceFileRel = None
-                elif inspect.ismodule(nameObj) and not hasattr(nameObj, '__file__'):
+                elif True and (inspect.ismodule(nameObj) and
+                               not hasattr(nameObj, '__file__')):
                     # built-in module (according to code in inspect.py)
                     sourceFileRel = None
-                elif inspect.isclass(nameObj) and not hasattr(sys.modules.get(nameObj.__module__), '__file__'):
+                elif True and (inspect.isclass(nameObj) and
+                               not hasattr(sys.modules.get(nameObj.__module__),
+                                           '__file__')):
                     # built-in class (according to code in inspect.py)
                     sourceFileRel = None
                 else:
@@ -270,10 +297,12 @@ def main(args):
                         sourceFileRel = inspect.getsourcefile(nameObj)
                     except TypeError:
                         sourceFileRel = None
-                        sys.stderr.write('NOTE: %s had no source file\n' % name)
+                        sys.stderr.write('NOTE: %s had no source file\n' %
+                                         name)
             if sourceFileRel is not None:
                 sourceFile = os.path.abspath(sourceFileRel)
-                if os.path.commonprefix([srcRootDir, sourceFile]) == srcRootDir:
+                if True and (os.path.commonprefix([srcRootDir, sourceFile]) ==
+                             srcRootDir):
                     namesWithinRoot.add(name)
 
     sys.stderr.write('\n')
@@ -284,50 +313,64 @@ def main(args):
 
     def nameToString(name):
         if name in namesNotYetDefined:
-            return 'ForwardRef(lambda: '+name+')'
+            return 'ForwardRef(lambda: %s)' % name
         else:
             return name
 
     # print stuff which occurs before the first node
-    bodyStartLine = (nodeModule.body[0].lineno - 1) if nodeModule.body else len(moduleFileLines)
+    bodyStartLine = ((nodeModule.body[0].lineno - 1) if nodeModule.body
+                     else len(moduleFileLines))
     for line in moduleFileLines[:bodyStartLine]:
         print line
 
-    for (node, nextNode), loadGlobals in zip(peekIter(nodeModule.body), loadGlobalss):
-        # (module docstrings which last more than one line seem to have col_offset -1)
+    for (node, nextNode), loadGlobals in zip(peekIter(nodeModule.body),
+                                             loadGlobalss):
+        # (module docstrings which last more than one line seem to have
+        #   col_offset -1)
         assert node.col_offset == 0 or node.col_offset == -1
         if nextNode is not None:
             assert nextNode.col_offset == 0
         startLine = node.lineno - 1
-        endLine = (nextNode.lineno - 1) if nextNode is not None else len(moduleFileLines)
+        endLine = ((nextNode.lineno - 1) if nextNode is not None
+                   else len(moduleFileLines))
         assert 0 <= startLine < endLine <= len(moduleFileLines)
         sourceLines = moduleFileLines[startLine:endLine]
 
         if isinstance(node, (_ast.FunctionDef, _ast.ClassDef)):
-            sortedDeps = [ nameToString(name) for name in loadGlobals if name in namesWithinRoot and name != node.name ]
+            sortedDeps = [ nameToString(name)
+                           for name in loadGlobals
+                           if name in namesWithinRoot and name != node.name ]
 
-            # work around the fact there is no completely reliable way to get the
-            #   line number of the first non-decorator line from the AST alone
+            # work around the fact there is no completely reliable way to get
+            #   the line number of the first non-decorator line from the AST
+            #   alone
             defLineOffset = None
             for lineOffset, line in enumerate(sourceLines):
                 if line.startswith('def ') or line.startswith('class '):
                     defLineOffset = lineOffset
                     break
             assert defLineOffset is not None
-            assert all([ (dec.lineno - 1) < startLine + defLineOffset for dec in node.decorator_list ])
+            assert all([ (dec.lineno - 1) < startLine + defLineOffset
+                         for dec in node.decorator_list ])
 
             codeDepsPrinted = False
 
             # print decorator lines
             for subNode, nextSubNode in peekIter(node.decorator_list):
-                if isinstance(subNode, _ast.Call) and isinstance(subNode.func, _ast.Name) and subNode.func.id == 'codeDeps':
+                if True and (isinstance(subNode, _ast.Call) and
+                             isinstance(subNode.func, _ast.Name) and
+                             subNode.func.id == 'codeDeps'):
                     # print new codeDeps stanza in place of old one
                     print prettyPrintBisqueDepsStanza(sortedDeps)
                     codeDepsPrinted = True
                 else:
                     startSubOffset = subNode.lineno - 1 - startLine
-                    endSubOffset = (nextSubNode.lineno - 1 - startLine) if nextSubNode is not None else defLineOffset
-                    assert 0 <= startSubOffset < endSubOffset <= len(sourceLines)
+                    endSubOffset = ((nextSubNode.lineno - 1 - startLine)
+                                    if nextSubNode is not None
+                                    else defLineOffset)
+                    assert (
+                        0 <= startSubOffset < endSubOffset <= len(sourceLines)
+                    )
                     for line in sourceLines[startSubOffset:endSubOffset]:
                         print line
             if not codeDepsPrinted:
@@ -339,11 +382,14 @@ def main(args):
             for line in sourceLines[defLineOffset:]:
                 print line
         else:
-            sortedDeps = [ nameToString(name) for name in loadGlobals if name in namesWithinRoot ]
+            sortedDeps = [ nameToString(name)
+                           for name in loadGlobals
+                           if name in namesWithinRoot ]
             if sortedDeps:
                 nameAssignedTo = simpleAssignToName(node)
                 if nameAssignedTo != None and nameAssignedTo.startswith('_'):
-                    # node is a simple assignment to a private variable, so don't need a codeDeps line
+                    # node is a simple assignment to a private variable, so
+                    #   don't need a codeDeps line
                     for line in sourceLines:
                         print line
                 else:
@@ -351,27 +397,40 @@ def main(args):
                         nodeValue = node.value
 
                         hadToRemoveExisting = False
-                        if isinstance(nodeValue, _ast.Call) and isinstance(nodeValue.func, _ast.Call) and isinstance(nodeValue.func.func, _ast.Name) and nodeValue.func.func.id == 'codeDeps':
+                        if True and (isinstance(nodeValue, _ast.Call) and
+                                     isinstance(nodeValue.func, _ast.Call) and
+                                     isinstance(nodeValue.func.func,
+                                                _ast.Name) and
+                                     nodeValue.func.func.id == 'codeDeps'):
                             # remove existing codeDeps stanza
-                            # (FIXME : assumes there is an arg (and that it comes before kwargs, etc, but I think that's safe))
+                            # (FIXME : assumes there is an arg (and that it
+                            #   comes before kwargs, etc, but I think that's
+                            #   safe))
                             nodeValue = nodeValue.args[0]
                             hadToRemoveExisting = True
 
                         currLineOffset = nodeValue.lineno - 1 - startLine
-                        restOfCurrLine = sourceLines[currLineOffset][nodeValue.col_offset:]
+                        restOfCurrLine = (
+                            sourceLines[currLineOffset][nodeValue.col_offset:]
+                        )
                         restOfLines = sourceLines[(currLineOffset + 1):]
                         if not hadToRemoveExisting:
                             # if codeDeps is already present, then user must
                             #   have added, so no need to warn
-                            print '# FIXME : to examine manually (assumes original RHS is function or class)'
-                        print prettyPrintBisqueDepsStanza(sortedDeps, init = nameAssignedTo+' = ')+'('
-                        print '    '+restOfCurrLine
+                            print ('# FIXME : to examine manually (assumes'
+                                   ' original RHS is function or class)')
+                        print prettyPrintBisqueDepsStanza(
+                            sortedDeps,
+                            init = nameAssignedTo+' = '
+                        )+'('
+                        print '    %s' % restOfCurrLine
                         for line in restOfLines:
                             print line
                         if not hadToRemoveExisting:
                             print ')'
                     else:
-                        print '# FIXME : to examine manually -- codeDeps(%s)' % (', '.join(sortedDeps))
+                        print ('# FIXME : to examine manually --'
+                               ' codeDeps(%s)' % (', '.join(sortedDeps)))
                         for line in sourceLines:
                             print line
             else:
