b0221e3421b536caba4146e1060606d10a063f03
Matt Shannon <matt.shannon@cantab.net>
2013-07-18 20:52:26 +0100

    add basic distutils packaging stuff
    Ignore-this: ff4b71bfeebb9ccb956d4356cda0e97e
    
    darcs-hash:20130718195226-3a4db-3bd54e75d5416625d790f9820e284070eb0a1f31

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+"""A distutils-based script for distributing and installing armspeech."""
+
+# Copyright 2011, 2012, 2013 Matt Shannon
+
+# This file is part of armspeech.
+# See `License` for details of license and warranty.
+
+
+from distutils.core import setup
+
+with open('README.markdown') as readmeFile:
+    long_description = readmeFile.read()
+
+setup(
+    name = 'armspeech',
+    version = '0.6.dev1',
+    description = 'Autoregressive probabilistic modelling for speech synthesis.',
+    url = 'http://github.com/MattShannon/armspeech',
+    author = 'Matt Shannon',
+    author_email = 'matt.shannon@cantab.net',
+    license = 'various open source licenses (see License file)',
+    packages = ['bisque', 'armspeech', 'armspeech.modelling', 'armspeech.speech', 'armspeech.util'],
+    # (there are some scripts in the bin directory, but none of them are
+    #   suitable for system-wide installation)
+    scripts = [],
+    # (not actually used for anything with distutils!)
+    requires = ['numpy', 'scipy', 'matplotlib', 'codedep'],
+    provides = ['bisque', 'armspeech'],
+    long_description = long_description,
+)
